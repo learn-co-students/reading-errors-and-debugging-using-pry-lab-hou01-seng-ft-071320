1b278a07b33fcbc16f8efe92cf6554eb83a8c8ef
Junaid Abbasi <juni.abbasi@icloud.com>
2020-06-14 08:40:15 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,7 +9,16 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+def crew
+  ["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."]
+end
+
+def greet_crew (crew)
+  crew.each {|crew_member| "Hello #{crew_member}."}
+end
+
 def engage
-  puts state_log(date)
   date = generate_star_date
+  state_log(date)
+  greet_crew(crew)
 end
